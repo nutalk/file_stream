12df559b1acff1d5e5e772d1559ba0357a3a740b
hetao <ht2005112@hotmail.com>
2020-02-13 14:24:30 +0800

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="file_stream",
+    version="0.0.1",
+    author="Nutalk",
+    author_email="ht2005112@hotmail.com",
+    description="process data as stream.",
+    license="BSD",
+    keywords="file",
+    url="https://github.com/nutalk/file_stream",
+    packages=find_packages(),
+    long_description="process data as stream.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
