e8e3eebcf94326e8d3082953bfd660609b7399d4
hetao <ht2005112@hotmail.com>
2020-02-13 15:28:58 +0800

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("readme.md", "r", encoding='utf-8') as fh:
+with open("readme.rst", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 setup(
     name="file_stream",
@@ -13,11 +13,17 @@ setup(
     url="https://github.com/nutalk/file_stream",
     packages=find_packages(),
     long_description=long_description,
+    install_requires=[
+            ],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
