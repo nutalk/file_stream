aef14866f5164fcbd50c0d027b83b79147e879ce
hetao <ht2005112@hotmail.com>
2020-02-13 15:02:49 +0800

    update

--- readme.md
+++ readme.md
@@ -1,8 +1,17 @@
 # 流式处理数据
 
 ## 原理说明
+
 编程主要要到了生成器，各个类用for循环从上游抽取数据，用yield给下游提供数据。通过改写or规则，将各个组建组合起来。
 
 ## 参考项目
+
 整体思路主要参考了这个项目：[python-stream](https://github.com/sandabuliu/python-stream)。
 
+## 使用
+> pip install file-stream
+```python
+from file-stream.executor.source import CsvReader
+from file-stream.executor.writer import CsvWriter
+
+```
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+with open("readme.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
 setup(
     name="file_stream",
     version="0.0.1",
@@ -10,10 +12,12 @@ setup(
     keywords="file",
     url="https://github.com/nutalk/file_stream",
     packages=find_packages(),
-    long_description="process data as stream.",
+    long_description=long_description,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
