78581807b0a28c05ab96d954489b6298879a0d67
hetao <ht2005112@hotmail.com>
2020-02-25 14:21:36 +0800

    增加删除重复内容的实现。

--- file_stream/filter.py
+++ file_stream/filter.py
@@ -24,3 +24,31 @@ class Filter(Executor):
             return item
         else:
             return None
+
+
+class DuplicateRemove(Executor):
+    def __init__(self, tell_fields: list):
+        """
+        删除重复内容。
+        :param tell_fields: 用来对比是否重复的列名称组成的列表。
+
+          remover = DuplicateRemove(['f_company_code', 'f_question'])
+        """
+        super().__init__()
+        self.tell_fields = tell_fields
+        self.exists = set()
+
+    def handle(self, item):
+        handler = tuple([item[field].replace('\n', '') for field in self.tell_fields])
+        if handler in self.exists:
+            return None
+        else:
+            return item
+
+    def __iter__(self):
+        for item in self._source:
+            result = self.handle(item)
+            if result is not None:
+                handler = tuple([item[field] for field in self.tell_fields])
+                self.exists.add(handler)
+                yield result
