621f300a6bc7e447743174e1d0f9ad1f4d1b6da0
hetao <ht2005112@hotmail.com>
2020-04-21 17:31:05 +0800

    update

--- file_stream/__init__.py
+++ file_stream/__init__.py
@@ -1,4 +1,4 @@
 # 在这里导入常用的包，方便导入。
 from file_stream.source import Dir, CsvReader, MysqlReader
 from file_stream.writer import MysqlWriter, CsvWriter
-from file_stream.filter import FinishedRemove, DuplicateRemove, Executor, FieldTrans, DataQC, NoneFiller
+from file_stream.filter import FinishedRemove, DuplicateRemove, Executor, FieldTrans, DataQC, NoneFiller, inspect_null
